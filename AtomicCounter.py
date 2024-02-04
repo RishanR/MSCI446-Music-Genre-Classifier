8b9a88df755ae098fbd20b36188e16b5b7052e4f
RishanRatna <rishanratna@hotmail.com>
2024-02-04 13:31:35 -0500

    create concurrent download manager for spotify preview mp3 downloads

---
+++ AtomicCounter.py
@@ -0,0 +1,21 @@
+import threading
+
+class AtomicCounter:
+    def __init__(self, initial=0):
+        self.value = initial
+        self._lock = threading.Lock()
+
+    def increment(self, num=1):
+        with self._lock:
+            self.value += num
+            return self.value
+
+# Example Usage:
+#
+# counter = AtomicCounter(0)
+#
+# counter.increment()
+# print(counter.value) - should be 1
+#
+# counter.increment(4)
+# print(counter.value) - should be 5
---
+++ ConcurrentDownloadManager.py
@@ -0,0 +1,45 @@
+import os
+import requests
+import threading
+import concurrent.futures
+from AtomicCounter import AtomicCounter
+
+class ConcurrentDownloadManager:
+    def __init__(self, num_threads=None):
+        self.num_threads = num_threads
+        self.totalFilesCount = 0
+        self.downloadedFilesCount = AtomicCounter(0)
+
+    def downloadFile(self, filename, url):
+        try:
+            result = requests.get(url)
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename, 'wb') as file:
+                file.write(result.content)
+            self.downloadedFilesCount.increment()
+            print(f'Downloaded files: {self.downloadedFilesCount.value}/{self.totalFilesCount}')
+        except Exception as e:
+            print(f'Exception in downloadFile({filename}, {url}):', e)
+    
+    def downloadFiles(self, files_to_download):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.num_threads) as executor:
+            self.totalFilesCount = len(files)
+            self.downloadedFilesCount = AtomicCounter(0)
+            for file in files_to_download:
+                (filename, url) = file
+                executor.submit(self.downloadFile, filename=filename, url=url)
+
+
+# Example Usage:
+#
+# download_manager = ConcurrentDownloadManager()
+#
+# files = [
+#     ("songs/symphony.mp3", "https://filesamples.com/samples/audio/mp3/Symphony%20No.6%20(1st%20movement).mp3"),
+#     ("songs/sample4.mp3", "https://filesamples.com/samples/audio/mp3/sample4.mp3"),
+#     ("songs/sample3.mp3", "https://filesamples.com/samples/audio/mp3/sample3.mp3"),
+#     ("songs/sample2.mp3", "https://filesamples.com/samples/audio/mp3/sample2.mp3"),
+#     ("songs/sample1.mp3", "https://filesamples.com/samples/audio/mp3/sample1.mp3")
+# ]
+#
+# download_manager.downloadFiles(files)
