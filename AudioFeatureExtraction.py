82a06d1c1387234499b9deec8ab5414eef0b3fe5
Pranavan <ppirahal@uwaterloo.ca>
2024-03-13 23:31:04 -0400

    fix the audiofeature extraction script

--- AudioFeatureExtraction.py
+++ AudioFeatureExtraction.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 import librosa
 from ConcurrentDownloadManager import ConcurrentDownloadManager
 
@@ -8,48 +9,41 @@ class AudioFeatureExtraction:
         self.data_df = data_df
     
     def _extract_mfcc_mean(self, audio_series, sample_rate):
-        mfcc = np.array(librosa.feature.mfcc(audio_series, sample_rate))
+        mfcc = np.array(librosa.feature.mfcc(y=audio_series, sr=sample_rate, n_mfcc= 13))
         return mfcc.mean(axis=1)
     
-    def _extract_spect_mean(self, audio_series, sample_rate):
-        spect = np.array(librosa.feature.melspectrogram(audio_series, sample_rate))
-        return spect.mean(axis=1)
-    
     def _extract_chroma_mean(self, audio_series, sample_rate):
-        chroma = np.array(librosa.feature.chroma_stft(audio_series, sample_rate))
+        chroma = np.array(librosa.feature.chroma_stft(y=audio_series, sr=sample_rate, n_chroma=12))
         return chroma.mean(axis=1)
     
-    def _extract_centroid_mean(self, audio_series, sample_rate):
-        centroid = np.array(librosa.feature.tonnetz(audio_series, sample_rate))
-        return centroid.mean(axis=1)
-    
     def extract_audio_features(self):
-        audio_features_dict = {
-            'id': [],
-            'mfcc_mean': [],
-            'spect_mean': [],
-            'chroma_mean': [],
-            'centroid_mean': []
-        }
+        audio_features_dict = defaultdict(list)
+
         for _, entry in self.data_df.iterrows():
             audio_features_dict['id'].append(entry['id'])
 
             audio_series, sample_rate = librosa.load(f"songs/{entry['id']}.mp3", offset=0, duration=30)
-            audio_features_dict['mfcc_mean'].append(self._extract_mfcc_mean(audio_series, sample_rate))
-            audio_features_dict['spect_mean'].append(self._extract_spect_mean(audio_series, sample_rate))
-            audio_features_dict['chroma_mean'].append(self._extract_chroma_mean(audio_series, sample_rate))
-            audio_features_dict['centroid_mean'].append(self._extract_centroid_mean(audio_series, sample_rate))
+            for i, val in enumerate(self._extract_mfcc_mean(audio_series, sample_rate)):
+                audio_features_dict[f"mfcc_{i+1}Mean"].append(val)
+            print("Finished MFCC extraction")
+
+            for i, val in enumerate(self._extract_chroma_mean(audio_series, sample_rate)):
+                audio_features_dict[f"chroma_{i+1}Mean"].append(val)
+            print("Finished Chroma extraction")
+
+
 
         return pd.DataFrame(audio_features_dict)
     
 data_df = pd.read_csv("data/filtered_music_data.csv")
 
-downloads_manager = ConcurrentDownloadManager()
-downloads_manager.downloadFiles([(f"songs/{entry['id']}.mp3", entry['preview_url']) for _, entry in data_df.iterrows()])
+print("Starting Audio Extraction...")
 
 audio_feature_extractor = AudioFeatureExtraction(data_df)
 audio_features_df = audio_feature_extractor.extract_audio_features()
 
+print("Finished Audio Extraction!")
+
 merged_df = pd.merge(data_df, audio_features_df, on='id')
-merged_df.to_csv("data/filtered_music_data_with_librosa")
+merged_df.to_csv("data/filtered_music_data_with_librosa.csv", index=False)
 
